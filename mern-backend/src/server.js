883336fdf42ee343282379cbbceedf708950a916
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-29 00:07:51 +0530

    Connection to MongoDB Atlas

--- mern-backend/src/server.js
+++ mern-backend/src/server.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const env = require('dotenv')
 const app = express()
+const mongoose = require('mongoose')
 
 // environment variable
 env.config() 
 
+//mongodb connection string
+const mongodbConnectionString = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@flipcart.ks697.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+
+mongoose.connect(mongodbConnectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+    console.log('Database connected ...')
+  })
+
+app.use(express.json())
+
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Hello from SERVER'
+  })
+})
+
+app.post('/data', (req, res, next) => {
+  res.status(200).json({
+    message: req.body
   })
 })
 
