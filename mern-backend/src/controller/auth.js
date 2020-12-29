810309f44dbbde1f6d7205ff0cf5d563583c20a6
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-30 00:53:00 +0530

    Sign In with JWT

---
+++ mern-backend/src/controller/auth.js
@@ -0,0 +1,62 @@
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+exports.signup = (req, res) => {
+
+    User.findOne({ email: req.body.email })
+    .exec((err, user) => {
+        if (user) return res.status(400).json({
+            message: "User already registered"
+        })
+
+        const { firstName, lastName, email, password } = req.body
+
+        const _user = new User({
+            firstName,
+            lastName,
+            email,
+            password,
+            username: Math.random().toString()
+        })
+
+        _user.save((err, data) => {
+            if (err) { 
+                return res.status(400).json({
+                    message: "Something went wrong"
+                })
+            }
+            if (data) {
+                return res.status(201).json({
+                    message: "User created successfully"
+                })
+            }
+        })
+    })
+}
+
+exports.signin = (req, res) => {
+    User.findOne({ email: req.body.email })
+        .exec((err, user) => {
+            if (err) return res.status(400).json({ err }) 
+            if (user) {
+                if (user.authenticate(req.body.password)) {
+                    const token = jwt.sign(
+                        { _id: user._id },
+                        process.env.JWT_SECRET_KEY,
+                        { expiresIn: '1h' }
+                    )
+                    const { _id, firstName, lastName, email, role, fullName } = user
+                    res.status(200).json({
+                        token,
+                        user: { _id, firstName, lastName, email, role, fullName }
+                    })
+                } else {
+                    res.status(400).json({
+                        message: 'Invalid password'
+                    })
+                }
+            } else {
+                res.status(400).json({ message: "Something went wrong" })
+            }
+        })
+}
--- mern-backend/src/server.js
+++ mern-backend/src/server.js
@@ -4,7 +4,7 @@ const app = express()
 const mongoose = require('mongoose')
 
 //routes
-const userRoutes = require('./routes/user')
+const authRoutes = require('./routes/auth')
 
 // environment variable
 env.config() 
@@ -23,7 +23,7 @@ mongoose.connect(mongodbConnectionString,
 
 app.use(express.json())
 
-app.use('/api', userRoutes)
+app.use('/api', authRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
