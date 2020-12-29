0ea914174b31579d87d5bd385984b48395d1fd14
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-30 01:50:14 +0530

    Admin Authentication

--- mern-backend/src/routes/auth.js
+++ mern-backend/src/routes/auth.js
@@ -5,8 +5,8 @@ const router = express.Router()
 router.post('/signup', signup)
 router.post('/signin', signin)
 
-router.post('/profile', requireSignin,  (req, res) => {
-    res.status(200).json({ user: 'profile' })
-})
+// router.post('/profile', requireSignin,  (req, res) => {
+//     res.status(200).json({ user: 'profile' })
+// })
 
 module.exports = router
--- mern-backend/src/server.js
+++ mern-backend/src/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 
 //routes
 const authRoutes = require('./routes/auth')
+const adminRoutes = require('./routes/admin/auth')
 
 // environment variable
 env.config() 
@@ -24,6 +25,7 @@ mongoose.connect(mongodbConnectionString,
 app.use(express.json())
 
 app.use('/api', authRoutes)
+app.use('/api', adminRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
