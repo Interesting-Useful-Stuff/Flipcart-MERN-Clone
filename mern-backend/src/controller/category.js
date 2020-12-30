35cb0827c18844ba4858c03ecb59482531783485
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-31 01:15:14 +0530

    Recursive NestedCategory function

--- mern-backend/src/controller/category.js
+++ mern-backend/src/controller/category.js
@@ -1,30 +1,51 @@
-const Category = require('../models/category')
-const slugify = require('slugify')
+const Category = require("../models/category");
+const slugify = require("slugify");
+
+const createNestedCategories = (categories, parentId = null) => {
+    const categoryList = [];
+    let category;
+    if (parentId == null) {
+        category = categories.filter(cat => cat.parentId == undefined)
+    } else {
+        category = categories.filter(cat => cat.parentId == parentId)
+    }
+
+    for (let cate of category) {
+        categoryList.push({
+            _id: cate._id,
+            name: cate.name,
+            slug: cate.slug,
+            children: createNestedCategories(categories, cate._id)
+        })
+    }
+    return categoryList;
+}
 
 exports.addCategory = (req, res) => {
     const categoryObj = {
         name: req.body.name,
-        slug: slugify(req.body.name)
-    }
+        slug: slugify(req.body.name),
+    };
     if (req.body.parentId) {
-        categoryObj.parentId = req.body.parentId
+        categoryObj.parentId = req.body.parentId;
     }
-    const cat = Category(categoryObj)
+    const cat = Category(categoryObj);
     cat.save((err, category) => {
-        if (err) return res.status(400).json({ err })
+        if (err) return res.status(400).json({ err });
         if (category) {
-            return res.status(200).json({ category })
+            return res.status(200).json({ category });
         }
-    })
-}
+    });
+};
 
 exports.getCategory = (req, res) => {
-    Category.find({})
-        .exec((err, category) => {
-            if (err) return res.status(400).json({ err })
-            if (category) {
-                return res.status(200).json({ category })
-            }
-        })
+    Category.find({}).exec((err, categories) => {
+        if (err) return res.status(400).json({ err });
 
-}
+        if (categories) {
+            const categoryList = createNestedCategories(categories);
+
+            return res.status(200).json({ categoryList });
+        }
+    });
+};
